dcebbb1e90cc0d7003f352324656f8382aaaf0c6
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-04-14 09:26:13 -0300

    pull semana 2

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -4,27 +4,25 @@ import { invalidDataError, notFoundError } from '@/errors';
 import addressRepository, { CreateAddressParams } from '@/repositories/address-repository';
 import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
 import { exclude } from '@/utils/prisma-utils';
-import { ViaCEPAddress } from '@/protocols';
+import { AddressEnrollment } from '@/protocols';
 
-async function getAddressFromCEP(cep: string) {
+async function getAddressFromCEP(cep: string): Promise<AddressEnrollment> {
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
-  if (!result.data) {
-    throw notFoundError();
+  if (!result.data || result.data.erro) {
+    throw notFoundError(); // lança um erro para quem chamou essa função!
   }
 
-  if (result.data.erro) {
-    throw notFoundError();
-  }
+  const { bairro, localidade, uf, complemento, logradouro } = result.data;
 
-  const { bairro, localidade, complemento, logradouro, uf } = result.data;
-  const address: ViaCEPAddress = {
+  const address: AddressEnrollment = {
     bairro,
     cidade: localidade,
+    uf,
     complemento,
     logradouro,
-    uf,
   };
+
   return address;
 }
 
